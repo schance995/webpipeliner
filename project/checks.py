e00d7e9d2db4b6f42d2ae9ccce5ccc86b0b8e46c
schance995 <54919210+schance995@users.noreply.github.com>
2020-08-18 02:06:16 -0400

    Added doc strings for checks.py and improved PEP8 compliance

--- project/checks.py
+++ project/checks.py
@@ -3,195 +3,282 @@ from os import walk
 from numbers import Number
 from werkzeug.utils import secure_filename
 
-# json dumps happens in routes
+
 def valid_filename(filename):
-    rgx = '^(.+)\.(R[12]\.fastq\.gz)$'
+    '''
+    Takes in a filename and determines if it is a valid fastq.gz filename.
+
+    Args:
+        filename (str): A filename
+
+    Returns:
+        valid (bool): True if the filename is valid, else False
+    '''
+    rgx = r'^(.+)\.(R[12]\.fastq\.gz)$'
+    # err = []
+    valid = True
     m = match(rgx, filename)
     if not m:
-        return (False, 'The filename {} should end with R1.fastq.gz or R2.fastq.gz'.format(filename))
+        valid = True
+        # err.append('The filename {} should end with R1.fastq.gz or R2.fastq.gz'.format(filename))
+        return valid
     else:
-        err = ''
+        '''
+        # filename reporting
         captured = m[1]
         if captured[0].isdigit():
-            err += '\nShould not start with a digit'
+            err.append('Should not start with a digit')
         if ' ' in captured:
-            err += '\nShould not contain a space'
+            err.append('Should not contain a space')
         if '-' in captured:
-            err += '\nShould not contain a hyphen/minus sign'
+            err.append('Should not contain a hyphen/minus sign')
         if 'sample' in captured:
-            err += '\nShould not contain "sample" before {}'.format(m[2])
+            err.append('Should not contain "sample" before {}'.format(m[2]))
         if 'R1' in captured:
-            err += '\nShould not contain "R1" before {}'.format(m[2])
+            err.append('Should not contain "R1" before {}'.format(m[2]))
         if 'R2' in captured:
-            err += '\nShould not contain "R2" before {}'.format(m[2])
-        if err != '':
+            err.append('Should not contain "R2" before {}'.format(m[2]))
+        if len(err):
             err = 'The filename %s has the following errors %s' % filename, err
-        return (err == '', err)
+        '''
+        return valid
+
 
 def read_data_dir(path):
-    
+    '''
+    Takes a directory and returns valid filenames and other special filenames inside.
+
+    Args:
+        path: a directory path
+
+    Returns:
+        rawdata (dict of list of str):
+            Maps filename prefixes to a list.
+            The list contains ints 1 and/or 2 to indicate single/paired-end filenames.
+            Note: If errors are present then this value is None.
+
+        paired_end (bool):
+            True if paired-end data is found, else False.
+            Note: If errors are present then this value is None.
+
+        err (bool):
+            True if errors are present, else False.
+
+        specialfound (list of str):
+            A list of special filenames present in the directory, as
+            specified in the function.
+                Note: If errors are present then this value is None.
+    '''
     err = ''
     rawdata = {}
     paired_end = False
     count = 0
-    rgx = '^(.*)\.R([12])\.fastq\.gz$'
-    specialfiles = ['contrasts.tab', 'contrast.tab', 'groups.tab', 'pairs.tab', 'peakcall.tab']
+    rgx = r'^(.*)\.R([12])\.fastq\.gz$'
+    specialfiles = ['contrasts.tab', 'contrast.tab',
+                    'groups.tab', 'pairs.tab',
+                    'peakcall.tab']
     specialfound = []
-    
-    # get only all (base) filenames to simulate the data directory
+
     # be sure to set limits on .. if needed
     try:
         (_, _, filenames) = next(walk(path))
         for f in filenames:
-            if f in specialfiles: specialfound.append(f)
+            if f in specialfiles:
+                specialfound.append(f)
             else:
-                valid, _ = valid_filename(f)
-                if valid:
+                valid = valid_filename(f)
+                if not valid:
                     count += 1
                     m = match(rgx, f)
-                    # m[1] = sample name, m[2] = 1 or 2
-                    if m[1] in rawdata: rawdata[m[1]].append(m[2])
-                    else: rawdata[m[1]] = [m[2]]
+                    # m[1] is sample name, m[2] is 1 or 2
+                    if m[1] in rawdata:
+                        rawdata[m[1]].append(m[2])
+                    else:
+                        rawdata[m[1]] = [m[2]]
 
-                    if m[2] == '2': paired_end = True
+                    if m[2] == '2':
+                        paired_end = True
 
-        if not count: # 0 files found
+        if not count:  # 0 files found
             err = 'The selected directory "{}" has no relevant fastq files.'.format(path)
-    except StopIteration: # dir doesn't exist
+    except StopIteration:  # dir doesn't exist
         err = 'The selected directory "{}" does not exist.'.format(path)
 
-    if err: return (None, None, err, None)
-    else: return (rawdata, paired_end, err, specialfound)
+    if err:
+        return (None, None, err, None)
+    else:
+        return (rawdata, paired_end, err, specialfound)
 
-'''
-if file is uploaded lines can be read first then sent to the function:
-F=open('groups.tab', 'r')
-lines=F.readlines()
-F.close()
-fun(lines) # a list of lines
-'''
 
-# TODO track multiple errors
-def read_groups(lines, inputdata):
-    # give a list of lines
-    samples = {} # dictionaries preserve insertion order
+def read_groups(lines, prefixes):
+    '''
+    Takes lines read from groups.tab and file prefixes from the data directory, validates the lines, then returns the file contents as a dict or a list of errors encountered.
+
+    Args:
+        lines (list of str):
+            Lines from a file, must be groups.tab
+        inputdata (list of str):
+            File prefixes from the data directory, must pass in read_data_dir output
+
+    Returns:
+        groups (dict of list of str):
+            Content in lines reorganized by samples, groups, and labels.        Each list is ordered by line number.
+            If errors are found, this value is None.
+        err (list of str):
+            A list of errors
+    '''
+    samples = {}  # dictionaries preserve insertion order
     labels = []
     line_num = 0
-    groups = {'samples':None, 'groups':None, 'labels':None} # default
+    groups = {'samples': None, 'groups': None, 'labels': None}
     err = []
 
     for line in lines:
         line_num += 1
         parts = [p for p in line.strip().split('\t') if p]
-        if len(parts): # ignore empty lines
+        if len(parts):  # ignore empty lines
             if len(parts) > 3:
                 err.append('Groups.tab, line {}: has too many columns, should have exactly 3 columns'.format(line_num))
                 continue
-                #break
             elif len(parts) < 3:
                 err.append('Groups.tab, line {}: has too few columns, should have exactly 3 columns'.format(line_num))
                 continue
-                #break
-            
+
             # exactly 3 parts
             name = parts[0]
             if name in samples:
-                if samples[name] == parts[1:]: # if the other 2 parts match then this line is a duplicate
+                # if the other 2 parts match then this line is a duplicate
+                if samples[name] == parts[1:]:
                     err.append('Line {}: is a duplicate: "{}"'.format(line_num, name))
-                continue # do not read in the duplicate group
+                continue  # do not read in the duplicate group
 
-            if name not in inputdata:
+            if name not in prefixes:
                 err.append('Groups.tab, {}: the sample name {} doesn\'t appear in your data directory'.format(line_num, name))
                 continue
-                #break
 
-            samples[name] = parts[1:] # group
+            samples[name] = parts[1:]  # group
 
-    if err: # == ''
+    if err:
         return (None, err)
     else:
         groups['samples'] = list(samples.keys())
-        groups['groups'], groups['labels'] = list(zip(*samples.values())) # may be multiple groups
-#        groups['rlabels']=labels
+        groups['groups'], groups['labels'] = list(zip(*samples.values()))
         return (groups, err)
-        # does this dictionary be named exactly this way?
 
-# code to verify groups.tab
-# groups.tab must exist, and the groups must exist in groups.tab.
-# assuming that a list of groups called groups exists
-'''
-if file is uploaded then can just read first line
-F=open('contrasts.tab', 'r')
-line=F.readline() # only need first line
-F.close()
-'''
-def read_contrasts(lines, samples):
+
+def read_contrasts(lines, groups):
+    '''
+    Takes lines read from contrasts.tab and sample names read from groups.tab, validates the lines, then returns the file contents as a list or a list of errors encountered.
+
+    Args:
+        lines (list of str):
+            Lines from a file, must be contrasts.tab
+        groups (list of str):
+            Group names from groups.tab, must pass in read_groups output
+
+    Returns:
+        contrasts (list of list of str):
+            Content in lines as a list format.
+            If errors are found, this value is None.
+        err (list of str):
+            A list of errors
+    '''
     err = []
     contrasts = []
     line_num = 0
     for line in lines:
         line_num += 1
         parts = [p for p in line.strip().split('\t') if p]
-        if parts in contrasts: # skip duplicates
+        if parts in contrasts:  # skip duplicates
             continue
-        if not len(parts): # skip blank lines
+        if not len(parts):  # skip blank lines
             continue
         if len(parts) > 4:
             err.append('Contrasts.tab, line {}: needs between 2 to 4 entries'.format(line_num))
             continue
-            # break
         elif len(parts) < 2:
             err.append('Contrasts.tab, line {}: needs between 2 to 4 entries'.format(line_num))
             continue
-            # break
-        
-        if parts[0] in samples and parts[1] in samples:
+
+        if parts[0] in groups and parts[1] in groups:
             while len(parts) < 4:
-                parts.append(0.5) # default values when not included
+                parts.append(0.5)  # default values when not included
             try:
                 parts[2] = float(parts[2])
                 parts[3] = float(parts[3])
                 contrasts.append(parts)
             except ValueError:
                 err.append('Contrasts.tab, line {}: columns 3 and 4 should be (small) numbers'.format(line_num))
-                
+
         else:
-            s = parts[0] if parts[0] not in samples else parts[1]
+            s = parts[0] if parts[0] not in groups else parts[1]
             err.append('Contrasts.tab, line {}: the sample {} doesn\'t exist in groups.tab'.format(line_num, s))
 
     return (contrasts, err)
 
-# each pair is unique, and the samples exist in the raw data directory. Ignore duplicate lines
-def read_pairs(lines, samples):
+
+def read_pairs(lines, prefixes):
+    '''
+    Takes lines read from pairs.tab and file prefixes from the data directory, validates the lines, then returns the file contents as a list or a list of errors encountered.
+
+    Args:
+        lines (list of str):
+            Lines from a file, must be pairs.tab
+        prefixes (list of str):
+            File prefixes from the data directory, must pass in read_data_dir output
+
+    Returns:
+        contrasts (list of list of str):
+            Content in lines as a list format.
+            If errors are found, this value is None.
+        err (list of str):
+            A list of errors
+    '''
     err = []
     pairs = []
-    tumors = [] # each tumor sample should appear only once
+    tumors = []  # each tumor sample should appear only once
     line_num = 0
 
     for line in lines:
         line_num += 1
         parts = [p for p in line.strip().split('\t') if p]
-        if parts in pairs: # skip duplicates
+        if parts in pairs:  # skip duplicates
             continue
-        if not len(parts): # skip blank lines
+        if not len(parts):  # skip blank lines
             continue
         if len(parts) == 1 or len(parts) > 2:
             err.append('Pairs.tab, line {}: needs 2 entries exactly').format(line_num)
             continue
-        if parts[0] in samples and parts[1] in samples:
+        if parts[0] in prefixes and parts[1] in prefixes:
             if parts[1] in tumors:
                 err.append('Pairs.tab, line {}: tumor "{}" must be unique').format(parts[1])
                 continue
             else:
                 pairs.append(parts)
         else:
-            s = parts[0] if parts[0] not in samples else parts[1]
+            s = parts[0] if parts[0] not in prefixes else parts[1]
             err.append('Pairs.tab, line {}: the sample name {} doesn\'t appear in your data directory'.format(line_num, s))
-    
+
     return (pairs, err)
 
-# reading peakcall.tab
-def read_peakcall(lines, samples):
+
+def read_peakcall(lines, prefixes):
+    '''
+    Takes lines read from peakcall.tab and file prefixes from the data directory, validates the lines, then returns the file contents as a list or a list of errors encountered.
+
+    Args:
+        lines (list of str):
+            Lines from a file, must be peakcall.tab
+        prefixes (list of str):
+            File prefixes from the data directory, must pass in read_data_dir output
+
+    Returns:
+        peaks (list of list of str):
+            Content in lines as a list format.
+            If errors are found, this value is None.
+        err (list of str):
+            A list of errors
+    '''
     err = []
     peaks = []
     line_num = 0
@@ -199,9 +286,9 @@ def read_peakcall(lines, samples):
     for line in lines:
         line_num += 1
         parts = [p for p in line.strip().split('\t') if p]
-        if parts in peaks: # skip duplicates
+        if parts in peaks:  # skip duplicates
             continue
-        if not len(parts): # skip blank lines
+        if not len(parts):  # skip blank lines
             continue
         if len(parts) != 3:
             err.append('Peakcall.tab, line {}: needs 3 entries exactly'.format(line_num))
@@ -214,8 +301,24 @@ def read_peakcall(lines, samples):
 
     return (peaks, err)
 
-# reading contrast.tab
-def read_contrast(lines, samples):
+
+def read_contrast(lines, groups):
+    '''
+    Takes lines read from contrast.tab and group names from peakcall.tab, validates the lines, then returns the file contents or a list of errors encountered.
+
+    Args:
+        lines (list of str):
+            Lines from a file, must be contrast.tab
+        groups (list of str):
+            Group names from peakcall.tab, must pass in read_peaks output
+
+    Returns:
+        contrast (list of list of str):
+            Content in lines as a list format.
+            If errors are found, this value is None.
+        err (list of str):
+            A list of errors
+    '''
     err = []
     contrast = []
     line_num = 0
@@ -223,13 +326,13 @@ def read_contrast(lines, samples):
     for line in lines:
         line_num += 1
         parts = [p for p in line.strip().split('\t') if p]
-        
-        if parts in contrast: # skip duplicates
+
+        if parts in contrast:  # skip duplicates
             continue
-            
-        if not len(parts): # skip blank lines
+
+        if not len(parts):  # skip blank lines
             continue
-            
+
         if len(parts) != 2:
             err.append('Contrast.tab, line {}: needs 2 entries exactly').format(line_num)
         else:
@@ -241,9 +344,24 @@ def read_contrast(lines, samples):
 
     return (contrast, err)
 
+
 def read_file(file, datatocompare):
+    '''
+    Takes a file and a set of comparison data, then tries to read the file with the comparison data and return the file contents or a list of errors.
+
+    Args:
+        file: the file to read
+        datatocompare: the data to be passed as an argument to the appropriate file reading function
+
+    Returns:
+        res: The contents of the file as returned by the file reading function, or None if there are errors.
+        err: A list of errors generated by the file reading function.
+
+    Raises:
+        ValueError: if the filename is not an exact match of special input files.
+    '''
     # file must be converted from bytes to string
-    name = secure_filename(file.filename) # strips .. and gets end filename
+    name = secure_filename(file.filename)  # strips .. and gets end filename
     lines = file.read().decode('utf-8').split('\n')
     func = None
     if name == 'contrast.tab':
@@ -257,15 +375,15 @@ def read_file(file, datatocompare):
     elif name == 'groups.tab':
         func = read_groups
     else:
-        raise ValueError('No read function for field '+name)
-    
+        raise ValueError('No read function for field ' + name)
+
     res, err = func(lines, datatocompare)
     if err:
         return (None, err)
     else:
         return (res, err)
-        
-    
+
+
 # TESTS
 if __name__ == '__main__':
     good_filenames = [
@@ -295,7 +413,7 @@ if __name__ == '__main__':
         '/data/CCBR_Pipeliner/testdata/rnaseq/expression_demo/TreatmentB_S73.R2.fastq.gz']
     test = [valid_filename(x) for x in good_filenames]
     assert(False not in [b for (b, dummy) in test])
-    
+
     invalid_filenames = ['.R1.fastq.gz',
                          'R2.fastq.gz',
                          '0test.R1.fastq.gz',
